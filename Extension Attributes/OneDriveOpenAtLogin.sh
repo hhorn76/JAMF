cd4c8a4217c4e6a41f3793d8c7c1a549cdb5f853
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-11-15 15:01:08 +0100

    Create OneDriveOpenAtLogin.sh

---
+++ Extension Attributes/OneDriveOpenAtLogin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Written by Heiko Horn 2018.11.15
+# Check if Onedrive is currently active
+MyResult=""
+fileName=""
+currentUser=$( /usr/bin/last -1 -t console | awk '{print $1}' )
+
+#If OneDrive is Standalone
+fileName=/Users/$user/Library/Preferences/com.microsoft.OneDrive.plist
+if [ -f "$fileName" ]; then
+	isSet=$(sudo -u $currentUser defaults read com.microsoft.OneDrive.plist OpenAtLogin)
+else
+	#If OneDrive is AppStore
+	fileName=/Users/$currentUser/Library/Containers/com.microsoft.OneDrive-mac/Data/Library/Preferences/com.microsoft.OneDrive-mac.plist
+	if [ -f "$fileName" ]; then
+		isSet=$(sudo -u $currentUser defaults read com.microsoft.OneDrive.plist OpenAtLogin)
+	fi;
+fi;
+if [ "${isSet}" = 1 ]; then
+	MyResult="TRUE"
+else
+	MyResult="FALSE"
+fi
+echo "<result>${MyResult}</result>"
