1539c6678075a434910664dae90b62ec898c476b
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-11-15 11:38:06 +0100

    Delete SecurityAskForPassword.skForPasswordDelay.sh

--- Extension_Attributes/SecurityAskForPassword.skForPasswordDelay.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Written by Heiko Horn 2017
-# Check if the interval of the "Require Password after sleep or screen saver begins" in the General Tab of the Security & Privacy System Preferences
-
-# Get current user
-currentUser=$(ls -la /dev/console | cut -d " " -f 4)
-# Get password deelay interval
-askForPasswordDelay=$(sudo -u $currentUser /usr/bin/defaults read com.apple.screensaver askForPasswordDelay)
-echo "<result>$askForPasswordDelay</result>"
