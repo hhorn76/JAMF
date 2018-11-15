a193e3e4aebb72445642f7867ef2b0d1c19655de
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-11-15 14:41:17 +0100

    Update ActiveDirectoryComputerDN.sh

--- Extension Attributes/ActiveDirectoryComputerDN.sh
+++ Extension Attributes/ActiveDirectoryComputerDN.sh
@@ -3,8 +3,8 @@
 # Gets the current distinguished name for a computer in active directory
 
 #Get computername and domain name
-compName=$(dsconfigad -show | awk '/Active Directory Domain/{print $NF}' | sed 's/$$//')
+computerName=$(dsconfigad -show | awk '/Computer Account/{print $NF}' | sed 's/$$//')
 domainName=$(dscl "/Active Directory/" read . SubNodes | awk '{print $2}')
 #Get DN for computer
-distinguishedName=$(dscl "/Active Directory/$domainName/All Domains" read /Computers/${compName}$ dsAttrTypeNative:distinguishedName | tail -1 | awk -F"${compName}," '{print $2}')
+distinguishedName=$(dscl "/Active Directory/${domainName}/All Domains" read /Computers/${computerName}$ dsAttrTypeNative:distinguishedName | tail -1 | awk -F"${computerName}," '{print $2}')
 echo "<result>$distinguishedName</result>"
