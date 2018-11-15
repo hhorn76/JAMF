0426d53123fa1eb0053173c0bbca6c66042337ad
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-11-15 14:41:20 +0100

    Update ActiveDirectoryComputerPasswordChangeInterval.sh

--- Extension Attributes/ActiveDirectoryComputerPasswordChangeInterval.sh
+++ Extension Attributes/ActiveDirectoryComputerPasswordChangeInterval.sh
@@ -2,6 +2,6 @@
 # Written by Heiko 2018.03.14
 # Will return the password change interval from Active Directory
 
-RESULT=$(dsconfigad -show | grep "Password change interval")
+RESULT=$(dsconfigad -show | grep "Password change interval" | awk '{print $5}')
 echo "<result>$RESULT</result>"
 exit 0
