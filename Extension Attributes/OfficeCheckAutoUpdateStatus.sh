49216f47991b86385a43cd326067a36f5522819f
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-12-05 11:07:34 +0100

    Update OfficeCheckAutoUpdateStatus.sh

--- Extension Attributes/OfficeCheckAutoUpdateStatus.sh
+++ Extension Attributes/OfficeCheckAutoUpdateStatus.sh
@@ -2,5 +2,7 @@
 # Written by Heiko Horn 2018.12.05
 # Check the status on how Office Updates are being deployed
 
-AutoUpdate=$(defaults read com.microsoft.autoupdate2 HowToCheck)
+currentUser=$( /usr/bin/last -1 -t console | awk '{print $1}' )
+
+AutoUpdate=$(sudo -u $currentUser defaults read com.microsoft.autoupdate2 HowToCheck)
 echo "<result>${AutoUpdate}</result>"
