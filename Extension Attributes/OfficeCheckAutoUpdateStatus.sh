aa99ca98e77db1d5c4c024f085b7145a1abca311
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-12-05 12:09:33 +0100

    Update OfficeCheckAutoUpdateStatus.sh

--- Extension Attributes/OfficeCheckAutoUpdateStatus.sh
+++ Extension Attributes/OfficeCheckAutoUpdateStatus.sh
@@ -6,3 +6,4 @@ currentUser=$( /usr/bin/last -1 -t console | awk '{print $1}' )
 
 AutoUpdate=$(sudo -u $currentUser defaults read com.microsoft.autoupdate2 HowToCheck)
 echo "<result>${AutoUpdate}</result>"
+exit 0
