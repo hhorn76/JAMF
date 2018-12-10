e1246a3bd5b7cfe64ca8fa7d4192470f1e2e5487
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-12-10 13:57:06 +0100

    Update osKillProcessConsumingCPU.sh

--- Scripts/osKillProcessConsumingCPU.sh
+++ Scripts/osKillProcessConsumingCPU.sh
@@ -2,8 +2,8 @@
 # Written by Heiko Horn 2018.12.10
 # This script will check if a process is running at more than 50%, if found will stopp the process and remove any preferences.
 
-processName="mdworker"
-processDisplayName="ParentalControls"
+processName="parentalcontrolsd"
+processDisplayName="ParentalControls" #Name for Error/Status messages.
 processUsage=$(ps ax -o %cpu,command -A | grep -v grep | grep $processName | awk '{print $1}')
 processPreferences=/Library/Application\ Support/Apple/ParentalControls 
 
@@ -11,7 +11,7 @@ if [ -z "${processUsage}" ]; then
 	echo "$processDisplayName process not found."
 else
 	echo "$processDisplayName CPU usage: $processUsage"
-	if [ ${processUsage%.*} -gt 50 ]; then 
+	if [ ${processUsage%.*} -gt 50 ]; then #Kill the process if CPU usage greater than 50%
 		echo "Killing $processName process running at $processUsage%"
 		pkill parentalcontrolsd
 		if [ -d $processPreferences ]; then
