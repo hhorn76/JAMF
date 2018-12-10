881df8f92f47a321ea30cc93ef7c779764a247f1
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2018-12-10 12:14:53 +0100

    Update OSProcessCpuUsage.sh

--- Extension Attributes/OSProcessCpuUsage.sh
+++ Extension Attributes/OSProcessCpuUsage.sh
@@ -2,8 +2,6 @@
 # Written by Heiko Horn 2018.12.05
 # Gets the CPU usage
 
-processName="parentalcontrolsd"
-pidNumber=$(ps ax | grep $processName | awk '{print $1}')
-RESULT=$(top -l 1 -pid $pidNumber | grep CPU | awk '{print $3}')
+RESULT=$(top -l 1 | grep CPU | grep -v %CPU | awk '{print $3}')
 echo "<result>$RESULT</result>"
 exit 0
