06cc1eea45446983ff433733742fc54645b50697
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2018-12-10 12:15:53 +0100

    Update osCpuUsage.sh

--- Extension Attributes/osCpuUsage.sh
+++ Extension Attributes/osCpuUsage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Written by Heiko Horn 2018.12.05
-# Gets the CPU usage
+# Gets the current CPU usage
 
 RESULT=$(top -l 1 | grep CPU | grep -v %CPU | awk '{print $3}')
 echo "<result>$RESULT</result>"
