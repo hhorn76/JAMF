fa894d4511f744720923cfca6b02484ed5cc7ecf
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2018-12-17 15:20:11 +0100

    Update ActiveDirectoryComputerLastPasswordChangeDays.sh

--- Extension Attributes/ActiveDirectoryComputerLastPasswordChangeDays.sh
+++ Extension Attributes/ActiveDirectoryComputerLastPasswordChangeDays.sh
@@ -4,7 +4,7 @@ domainName=$(dscl "/Active Directory/" read . SubNodes | awk '{print $2}')
 ADPWTIME=$(/usr/bin/security find-generic-password -s "/Active Directory/$domainName" /Library/Keychains/System.keychain 2> /dev/null | grep mdat | awk '{print substr($2, 2, length($2)-7)}')
 
 if [[ $ADPWTIME -gt 0 ]]; then
-	d1=$(date -j -f "%a %b %d %T %Z %Y" "$(date)" "+%s")
+	d1=$(date "+%s")
 	d2=$(date -j -f "%Y%m%d%H%M%S" "$ADPWTIME" "+%s")
         result=$(( ($d1 - $d2) / 86400 ))
 	echo "$d1 - $d2 / 86400 = $result"
