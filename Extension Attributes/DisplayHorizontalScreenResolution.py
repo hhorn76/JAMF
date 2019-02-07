72decaa599194a4342b968a6ffdbd72051c7f7f4
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-02-07 14:29:03 +0100

    Create DisplayHorizontalScreenResolution.py

---
+++ Extension Attributes/DisplayHorizontalScreenResolution.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# Written by Heiko 2019.02.07
+# Gets the horizontal screen resolution
+
+from AppKit import NSScreen
+print '<result>' + str ( int (NSScreen.mainScreen().frame().size.width ) ) + '</result>'
+
+print str ( int (NSScreen.mainScreen().frame().size.width ) )
