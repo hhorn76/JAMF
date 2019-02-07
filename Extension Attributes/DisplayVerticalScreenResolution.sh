89409cc6e132b97efa1640bed2deaef5f46770fd
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-02-07 14:29:14 +0100

    Update DisplayVerticalScreenResolution.sh

--- Extension Attributes/DisplayVerticalScreenResolution.sh
+++ Extension Attributes/DisplayVerticalScreenResolution.sh
@@ -1,6 +1,7 @@
-!#/bin/bash
+#!/bin/bash
 # Written by Heiko 2018.11.15
 # Gets the vertical screen resolution
 
-echo "<result>$(system_profiler SPDisplaysDataType | awk '/Resolution/{print $4}')</result>"
+strResulution=$(/usr/bin/python -c 'from AppKit import NSScreen; print str ( int (NSScreen.mainScreen().frame().size.height ) )')
+echo "<result>$strResulution</result>"
 exit 0
