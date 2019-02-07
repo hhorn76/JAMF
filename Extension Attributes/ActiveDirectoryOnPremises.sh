3129fb7d9dc7590477d5289caf4ed493fb783d78
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-02-07 14:28:50 +0100

    Update ActiveDirectoryOnPremises.sh

--- Extension Attributes/ActiveDirectoryOnPremises.sh
+++ Extension Attributes/ActiveDirectoryOnPremises.sh
@@ -2,7 +2,7 @@
 # Written by Heiko 2018.10.19
 # Checks if a computer is on premises by pinging the domain controllers
 
-dnsDOMAIN=""
+dnsDOMAIN=$(dsconfigad -show | awk '/Active Directory Domain/{print $NF}')
 if ping -c 2 -o $dnsDOMAIN; then
         result="Yes"
 else
