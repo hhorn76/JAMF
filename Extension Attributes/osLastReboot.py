6ebd9a37cf84aeee02f0173f5bf693fc78b9566f
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2019-02-07 21:45:17 +0100

    Update osLastReboot.py

--- Extension Attributes/osLastReboot.py
+++ Extension Attributes/osLastReboot.py
@@ -4,6 +4,8 @@
 
 from datetime import datetime
 import subprocess
+
 intTime = int (subprocess.check_output(['sysctl', '-h', 'kern.boottime']).strip().split()[4].replace(',',''))
 strTime = datetime.fromtimestamp(intTime).strftime("%Y-%m-%d %T")
+
 print '<result>' + strTime + '</result>'
