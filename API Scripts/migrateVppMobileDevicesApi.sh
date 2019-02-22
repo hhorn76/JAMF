36507cf79ab3f58caaee49305e1088d12b4fc721
Heiko Horn <40029809+hhorn76@users.noreply.github.com>
2019-02-22 21:17:10 +0100

    Create migrateVppMobileDevicesApi.sh

---
+++ API Scripts/migrateVppMobileDevicesApi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Written by Heiko Horn on 2019.02.22
+# This script migrates the moblile device application VPP token id, it can be used to migrate all applications from the egacy vpp to the new apps and books.
+
+# API service account credentials
+jamfUrl=$( defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url ) 
+# Base64 encoded username and password
+# echo apiuser:password | base64
+strAuth=''
+
+# XML sring to set the new vpp_admin_account_id to (Please modify to your need)
+strXml='<mobile_device_application><vpp><vpp_admin_account_id>3</vpp_admin_account_id></vpp></mobile_device_application>'
+
+# collect all application id's in an array
+arrID=$( curl -sk -H "authorization: Basic ${strAuth}" -H 'Accept: application/xml' ${jamfUrl}/JSSResource/mobiledeviceapplications | xmllint -xpath /mobile_device_applications/mobile_device_application/id - | sed -e 's/<[^>]*>/ /g' )
+
+for app in ${arrID[@]}; do
+# get the vpp id currently assigned
+intVpp=$( curl -sk -H "authorization: Basic ${strAuth}" -H 'Accept: application/xml' ${jamfUrl}/JSSResource/mobiledeviceapplications/id/${app}/subset/VPP | xmllint -xpath /mobile_device_application/vpp/vpp_admin_account_id - | sed -e 's/<[^>]*>//g' )
+
+if [ ${intVpp} -eq 1 ]; then
+echo -n Updating application ID: ${app}
+# set the new vpp_admin_account_id with the specified xml string
+curl -sk -H "authorization: Basic ${strAuth}" "${jamfUrl}/JSSResource/mobiledeviceapplications/id/${app}" -X PUT -H Content-type:application/xml --data $strXml echo ''
+fi
+done
