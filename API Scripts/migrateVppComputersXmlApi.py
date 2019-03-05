a9047c3b55cf4a5311ad2baa322daf3b24e51bda
Heiko Horn <hhorn@macOS-1013.mis-munich.de>
2019-03-06 00:20:36 +0100

    Create migrateVppComputersXmlApi.py

---
+++ API Scripts/migrateVppComputersXmlApi.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import urllib2, base64, plistlib
+import xml.etree.ElementTree as ET
+
+strJamfUrl = plistlib.readPlist('/Library/Preferences/com.jamfsoftware.jamf.plist')['jss_url'] + '/JSSResource'
+#strJamfUrl = 'https://xxx.xxx.xxx:8443/JSSResource'
+base64Auth=''
+
+request = urllib2.Request(strApiUrl)
+request.add_header('Authorization', 'Basic ' + base64Auth)
+request.add_header('Accept', 'application/xml')
+result = urllib2.urlopen(request)
+
+root = ET.fromstring(result.read())
+
+for child in root:
+	element = child.findall('./id')
+	for i in element:
+		intId = i.text
+		strApiUrlApp = strApiUrl + '/id/' + str (intId)
+		appRequest=urllib2.Request(strApiUrlApp)
+		appRequest.add_header('Authorization', 'Basic ' + base64Auth)
+		appRequest.add_header('Accept', 'application/xml')
+		resultApp = urllib2.urlopen(appRequest)
+		rootApp = ET.fromstring(resultApp.read())
+		print ('ID: ' + intId)
+		for name in rootApp.findall("./general/name"):
+			print('Name: ' + name.text)
+		for vppId in rootApp.findall("./vpp/vpp_admin_account_id"):
+			print('Vpp account Id: ' + vppId.text)
+		for vppTotal in rootApp.findall("./vpp/total_vpp_licenses"):
+			print('Tolal licenses: ' + vppTotal.text)
+		for vppUsed in rootApp.findall("./vpp/used_vpp_licenses"):
+			print('Used licenses: ' + vppUsed.text)
+		for vppFree in rootApp.findall("./general/is_free"):
+			print('Free: ' + vppFree.text)	
+		print ('')
+
+		
