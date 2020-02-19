28f56554151776972ef320a9418b2810bfcc19b9
Heiko Horn <hhorn76@gmail.com>
2020-02-19 10:08:51 +0100

    Create ApiDevicesRestartAppleTV.sh

---
+++ API Scripts/ApiDevicesRestartAppleTV.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Written by Heiko Horn on 2019.02.19
+# This script will filter all appleTV objects in Jamf an then sensd a restart MDM commands.
+
+# API service account credentials
+jamfUrl=$( defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url ) 
+# Base64 encoded username and password, please use the following command to get the strAuth value:
+# strAuth=$(echo apiuser:password | base64)
+strAuth=''
+
+function getAllComputers () {
+	/usr/bin/curl -sk -H "authorization: Basic ${strAuth}" -H 'Accept: application/xml' "${jamfUrl}/JSSResource/mobiledevices" -X GET -H "accept: application/xml" #| xmllint --format - | awk -F'>|<' '/<mobile_device>/{print $3}' | sort -n
+}
+
+function restartDevice () {
+	strStatus=$(/usr/bin/curl -sk -H "authorization: Basic ${strAuth}" -H 'Accept: application/xml' "${jamfUrl}/"JSSResource/mobiledevicecommands/command/RestartDevice/id/"${ID}" -X POST | xmllint --format - | awk -F'>|<' '/<status>/{print $3}')
+}
+
+# Get all mobile devices from Jamf
+strXml=$(getAllComputers)
+
+#Create arrays for Id's, Names and Models
+arrModel=( $(echo ${strXml} | xmllint --format - | awk -F'>|<' '/<model_identifier>/{print $3}') )
+arrId=( $(echo ${strXml} | xmllint --format - | awk -F'>|<' '/<id>/{print $3}') )
+strName=$(echo -en ${strXml} | xmllint --format - | awk -F'>|<' '/<name>/{print $3}')
+# This handles space characters for the names of the AppleTv's 
+oldIFS="$IFS" 
+IFS='
+'
+arrName=( $strName )
+IFS="$oldIFS"
+
+for i in "${!arrModel[@]}"; do
+	if [[ ${arrModel[i]} == AppleTV* ]]; then
+		ID="${arrId[i]}"
+		restartDevice
+		echo "ID: ${ID}"
+		echo "Name: ${arrName[i]}"
+		echo "Model: ${arrModel[i]}"
+		echo "Status: ${strStatus}"
+		echo ''
+	fi
+done
