35b81f305ba58f07c133693deb7d3a25fc82ed4f
Heiko Horn <hhorn76@gmail.com>
2020-10-17 19:29:04 +0200

    Update addInputMenuItem.sh

--- Scripts/addInputMenuItem.sh
+++ Scripts/addInputMenuItem.sh
@@ -3,6 +3,15 @@
 ### Written by Heiko Horn 2019.04.02
 ### This script will add the input menu icon to the menu itemes
 
+# Check if a variable is passed to the script otherwise use the current user name
+if [[ -z "${4}" ]]; then
+    USER=$( /usr/bin/stat -f%Su /dev/console )
+else
+    USER="${4}"
+fi
+
+PLIST="/Users/${USER}/Library/Preferences/com.apple.HIToolbox.plist"
+
 INPUT=$(/usr/libexec/PlistBuddy -c "Print :menuExtras" ${PLIST} | /usr/bin/grep -n "TextInput" | /usr/bin/awk -F ":" '{print $1}')
 
 if [ ! -z "${INPUT}" ]; then
